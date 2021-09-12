d283e3ad32687b39f744a9d011f06b849b91a192
madsankern <jqk218@econ.ku.dk>
2021-09-12 15:46:15 +0200

    Initial commit

---
+++ egm.py
@@ -0,0 +1,43 @@
+######################
+## Solver using EGM ##
+######################
+
+def solve(sol, par, c_next, m_next):
+
+    # Copy last iteration of the value function
+    v_old = sol.v.copy()
+
+    # Expand exogenous asset grid
+    a = np.tile(par.grid_a, np.size(par.y)) # 2d end-of-period asset grid
+
+    # m_plus = (1+par.r)
+
+    # Loop over exogneous states (post decision states)
+    for a_i,a in enumerate(par.grid_a):
+        
+        #Next periods assets and consumption
+        m_plus = (1+par.r)*a + np.transpose(par.y) # Transpose for dimension to fit
+
+        # Interpolate next periods consumption - can this be combined?
+        c_plus_1 = tools.interp_linear_1d(m_next[0,:], c_next[0,:], m_plus) # State 1
+        c_plus_2 = tools.interp_linear_1d(m_next[1,:], c_next[1,:], m_plus) # State 2
+
+        #Combine into a vector. Rows indicate income state, columns indicate asset state
+        c_plus = np.vstack((c_plus_1, c_plus_2))
+
+        # Marginal utility
+        marg_u_plus = util.marg_u(c_plus,par)
+        # Compute expectation below
+        av_marg_u_plus = np.array([par.P[0,0]*marg_u_plus[0,0] + par.P[0,1]*marg_u_plus[1,1], par.P[1,1]*marg_u_plus[1,1] + par.P[1,0]*marg_u_plus[0,0]])
+        # Add optimal consumption and endogenous state
+        sol.c[:,a_i+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+        sol.m[:,a_i+1] = a + sol.c[:,a_i+1]
+        sol.v = util.u(sol.c,par)
+
+    #Compute value function and update iteration parameters
+    sol.delta = max( max(abs(sol.v[0] - v_old[0])), max(abs(sol.v[1] - v_old[1])))
+    sol.it += 1
+
+    # sol.delta = max( max(abs(sol.c[0] - c_next[0])), max(abs(sol.c[1] - c_next[1])))
+
+    return sol
