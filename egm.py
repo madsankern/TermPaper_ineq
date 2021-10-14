84f3d61db3446b3be634082d01145cfa6e872e45
madsankern <jqk218@econ.ku.dk>
2021-10-14 08:37:22 +0200

    Added stochastic returns

--- egm.py
+++ egm.py
@@ -18,6 +18,8 @@ import utility as util
 ## Solver using EGM ##
 ######################
 
+# Write this better!
+
 def solve(sol, par, c_next, m_next):
 
     # Copy last iteration of the value function
@@ -31,22 +33,33 @@ def solve(sol, par, c_next, m_next):
     # Loop over exogneous states (post decision states)
     for a_i,a in enumerate(par.grid_a):
 
-        #Next periods assets and consumption
-        m_plus = (1+par.r)*a + np.transpose(par.y) # Transpose for dimension to fit
+        # Compute expected assets in the next period 
+        # m_plus = (1+par.r)*a + np.transpose(par.y) # Transpose for dimension to fit
 
-        # Interpolate next periods consumption - can this be combined?
-        c_plus_1 = tools.interp_linear_1d(m_next[0,:], c_next[0,:], m_plus) # State 1
-        c_plus_2 = tools.interp_linear_1d(m_next[1,:], c_next[1,:], m_plus) # State 2
+        # E_m_plus = np.sum(par.w*par.eps)*a + np.transpose(par.y)
 
-        #Combine into a vector. Rows indicate income state, columns indicate asset state
-        c_plus = np.vstack((c_plus_1, c_plus_2))
+        # # Interpolate next periods consumption - can this be combined?
+        # c_plus_1 = tools.interp_linear_1d(m_next[0,:], c_next[0,:], E_m_plus) # State 1
+        # c_plus_2 = tools.interp_linear_1d(m_next[1,:], c_next[1,:], E_m_plus) # State 2
+
+        # #Combine into a vector. Rows indicate income state, columns indicate asset state
+        # c_plus = np.vstack((c_plus_1, c_plus_2))
 
         # Marginal utility
-        marg_u_plus = util.marg_u(c_plus,par)
-        # Compute expectation below
-        av_marg_u_plus = np.array([par.P[0,0]*marg_u_plus[0,0] + par.P[0,1]*marg_u_plus[1,1], par.P[1,1]*marg_u_plus[1,1] + par.P[1,0]*marg_u_plus[0,0]])
+        # marg_u_plus = util.marg_u(c_plus,par) # This must be recomputed using the Guass Hermite weights!!!
+
+        # Find expected marginal utility in the next period, given each realization of y_t+1
+        marg_u_plus_1 = np.sum(par.w*par.eps * util.marg_u(tools.interp_linear_1d(m_next[0,:], c_next[0,:], par.eps*a + par.y1), par))
+        marg_u_plus_2 = np.sum(par.w*par.eps * util.marg_u(tools.interp_linear_1d(m_next[1,:], c_next[1,:], par.eps*a + par.y2), par))
+
+        # Combine
+        marg_u_plus = np.vstack((marg_u_plus_1, marg_u_plus_2))
+
+        # Compute the final expectation using the markov process for income
+        av_marg_u_plus = np.array([par.P[0,0]*marg_u_plus_1 + par.P[0,1]*marg_u_plus_2, par.P[1,1]*marg_u_plus_2 + par.P[1,0]*marg_u_plus_1])
+
         # Add optimal consumption and endogenous state
-        sol.c[:,a_i+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+        sol.c[:,a_i+1] = util.inv_marg_u(par.beta*av_marg_u_plus,par)
         sol.m[:,a_i+1] = a + sol.c[:,a_i+1]
         sol.v = util.u(sol.c,par)
 
